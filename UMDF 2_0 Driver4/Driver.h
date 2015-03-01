02dc6524f65933c35eeb48e54a81c339f1bdaf42
kr2000 <kr2000@users.noreply.github.com>
2015-02-28 20:08:12 -0800

    Add driver4 sample

---
+++ UMDF 2_0 Driver4/Driver.h
@@ -0,0 +1,33 @@
+/*++
+
+Module Name:
+
+    driver.h
+
+Abstract:
+
+    This file contains the driver definitions.
+
+Environment:
+
+    User-mode Driver Framework 2.0
+
+--*/
+
+#define INITGUID
+
+#include <windows.h>
+#include <wdf.h>
+
+#include "device.h"
+#include "queue.h"
+#include "trace.h"
+
+//
+// WDFDRIVER Events
+//
+
+DRIVER_INITIALIZE DriverEntry;
+EVT_WDF_DRIVER_DEVICE_ADD UMDF2_0Driver4EvtDeviceAdd;
+EVT_WDF_OBJECT_CONTEXT_CLEANUP UMDF2_0Driver4EvtDriverContextCleanup;
+
